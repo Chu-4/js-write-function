5adca6886e6f92cb5b025430fcd93bc1b0d77e9a
Ch-four <506341435@qq.com>
2021-08-25 07:39:18 +0800

    函数柯里化

---
+++ currying.js
@@ -0,0 +1,21 @@
+function currying(){
+    let res = [...arguments].reduce((a, b) => a+b)
+    function temp() {
+        if (arguments.length) {
+            res+=[...arguments].reduce((a, b) => a+b)
+            return temp
+        } else {
+            return res  // console.log(currying(1)(3)())或currying(1)(3) -> 4
+        }
+    }
+
+    temp.toString = function() { // 重写toSting() 方法, 为了console.log(currying(1)(3)) -> 4
+        console.log(res); 
+    }  
+    return temp
+}
+
+console.log(currying(1)(3)) // fn 4
+console.log(currying(1,3)) // fn 4
+currying(1)(3)() // 4
+currying(1,3)() // 4
